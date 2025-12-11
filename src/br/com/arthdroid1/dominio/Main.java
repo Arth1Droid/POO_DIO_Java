ad1a81f375d2f226a4f1882d7e3f8bb54ef46c83
ArthDroid1 <barbosarthur7@gmail.com>
2025-12-11 20:23:26 -0300

    Finaliza desafio

--- src/br/com/arthdroid1/dominio/Main.java
+++ src/br/com/arthdroid1/dominio/Main.java
@@ -4,19 +4,46 @@ public class Main {
 
 	
 	public static void main(String[] args) {
-		Curso curso = new Curso();
-		curso.setTitulo("POO EM JAVA");
-		curso.setDescricao("Curso Java");
-		curso.setCargaHoraria(12);
+		Curso curso1 = new Curso();
+		curso1.setTitulo("POO EM JAVA");
+		curso1.setDescricao("Curso Java");
+		curso1.setCargaHoraria(12);
 		Mentoria mentoria = new Mentoria();
 		mentoria.setTitulo("Mentoria de IA");
 		mentoria.setDescricao("Mentoria com especialista em IA");
 		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp JAVA");
+		bootcamp.setDescricao("Bootcamp para desenvolvedores java");
+		bootcamp.getConteudo().add(curso1);
+		bootcamp.getConteudo().add(mentoria);
 		
+		Dev devArthur = new Dev();
+		devArthur.setNome("Arthur");
+		devArthur.inscreverBootcamp(bootcamp);
+		System.out.println("Meus conteudos {Arthur}: " + devArthur.getConteudosInscritos());
+		devArthur.progredir();
+		System.out.println("Conteudos concluidos {Arthur}: " + devArthur.getConteudosConcluidos());
+		System.out.println("------------------------");
+		System.out.println("Meus conteudos {Arthur}: " + devArthur.getConteudosInscritos());
+		System.out.println("XP" + devArthur.calcularTotalXp() );
 
 		
-		System.out.println(curso);
-		System.out.println(mentoria);
+		Dev devArthDroid = new Dev();
+		devArthDroid.setNome("ArthDroid1");
+		devArthDroid.inscreverBootcamp(bootcamp);
+		System.out.println("Meus conteudos {ArthDroid1}: " + devArthDroid.getConteudosInscritos());
+		devArthDroid.progredir();
+		devArthDroid.progredir();
+		System.out.println("Conteudos concluidos {ArthDroid1}: " + devArthDroid.getConteudosConcluidos());
+		System.out.println("------------------------");
+		System.out.println("Meus conteudos {ArthDroid1}: " + devArthDroid.getConteudosInscritos());
+		System.out.println("XP" + devArthDroid.calcularTotalXp());
+
+
+
+		
+
 
 		
 	}
